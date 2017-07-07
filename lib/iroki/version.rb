57387d5a864a1d457baedc642fa32bf2cb16f9aa
Ryan Moore <moorer@udel.edu>
2017-07-07 19:03:25 -0400

    Bump version

--- lib/iroki/version.rb
+++ lib/iroki/version.rb
@@ -18,7 +18,7 @@
 
 # Library metadata
 module Iroki
-  VERSION = "0.0.35"
+  VERSION = "0.0.36"
   COPYRIGHT = "2015 - 2017 Ryan Moore"
   CONTACT   = "moorer@udel.edu"
   WEBSITE   = "https://github.com/mooreryan/iroki"
